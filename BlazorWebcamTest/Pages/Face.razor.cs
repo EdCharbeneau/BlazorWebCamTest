1c0658b5ba363459584e66c6fed82b70d45f44ee
Ed Charbeneau <ed.charbeneau@gmail.com>
2021-02-11 14:41:24 -0500

    Added shuffle

--- BlazorWebcamTest/Pages/Face.razor.cs
+++ BlazorWebcamTest/Pages/Face.razor.cs
@@ -4,31 +4,67 @@ using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Telerik.Blazor.Components;
 using WebcamComponent;
+using static LanguageExt.Prelude;
 
 namespace BlazorWebcamTest.Pages
 {
     public partial class Face
     {
+        // Dependencies
         [Inject] AzureSettings Settings { get; set; }
+
+        // Component References
         TelerikNotification ErrorNotification { get; set; }
-        bool WindowVisible { get; set; }
-        bool DevMode = true;
         Webcam Camera { get; set; }
-        List<(string Data, DetectedFace DetectedFace)> faces = new();
 
-        int index;
+        // State
+        bool windowVisible;
+        bool devMode = true;
+        readonly List<(string Data, DetectedFace DetectedFace)> faces = new();
+        int imageIndex;
 
+        async Task StartAnalysis()
+        {
+            var shuffledImageIndicies = Enumerable.Range(1, 8).Suffle();
+            windowVisible = true;
+            foreach (var i in shuffledImageIndicies)
+            {
+                imageIndex = i;
+                StateHasChanged();
+                await Task.Delay(160); // wait for reaction
+                await GetSnap();
+                StateHasChanged();
+                await Task.Delay(100); // rest period
+            }
+            windowVisible = false;
+        }
         async Task GetSnap()
         {
             var data = await Camera.GetSnapshot();
-            var face = await UploadAndDetectFaces(data);
-            faces.Add(face);
+            await TryUploadAndDetectFaces(data);
+        }
+        private async Task TryUploadAndDetectFaces(string base64encodedstring)
+        {
+            await TryAsync(() => UploadAndDetectFaces(base64encodedstring))
+                .Match(
+                    Succ: (face) => faces.Add((base64encodedstring, face)),
+                    Fail: (e) => ErrorNotification.Show(new()
+                    {
+                        Closable = true,
+                        Text = e.Message,
+                        ThemeColor = Telerik.Blazor.ThemeColors.Error,
+                        Icon = "exclamation-circle",
+                        ShowIcon = true,
+                        CloseAfter = 0
+                    })
+                );
         }
 
-        private async Task<(string, DetectedFace)> UploadAndDetectFaces(string base64encodedstring)
+        private async Task<DetectedFace> UploadAndDetectFaces(string base64encodedstring)
         {
             IFaceClient faceClient = new FaceClient(new ApiKeyServiceClientCredentials(Settings.Key)) { Endpoint = Settings.Uri };
 
@@ -42,62 +78,21 @@ namespace BlazorWebcamTest.Pages
                 };
 
             // Call the Face API.
-            try
-            {
-                // data:[<mediatype>][;base64],<data>
-                // Using split we can remove the data header and get the bytes
-                var bytes = Convert.FromBase64String(base64encodedstring.Split(',')[1]);
-                var imageFileStream = new MemoryStream(bytes);
-                // Whatever else needs to be done here.
-                IList<DetectedFace> faceList =
-                        await faceClient.Face.DetectWithStreamAsync(
-                            imageFileStream, true, false, faceAttributes);
-                return (base64encodedstring, faceList[0]);
-            }
-            // Catch and display Face API errors.
-            catch (APIErrorException f)
-            {
-                ErrorNotification.Show(new()
-                {
-                    Closable = true,
-                    Text = f.Message,
-                    ThemeColor = Telerik.Blazor.ThemeColors.Error,
-                    Icon = "exclamation-circle",
-                    ShowIcon = true,
-                    CloseAfter = 0
-                });
-                return ("", new DetectedFace());
-            }
-            // Catch and display all other errors.
-            catch (Exception e)
-            {
-                ErrorNotification.Show(new()
-                {
-                    Closable = true,
-                    Text = e.Message,
-                    ThemeColor = Telerik.Blazor.ThemeColors.Error,
-                    Icon = "exclamation-circle",
-                    ShowIcon = true,
-                    CloseAfter = 0
-                });
-                return ("", new DetectedFace());
-            }
+            // data:[<mediatype>][;base64],<data>
+            // Using split we can remove the data header and get the bytes
+            var bytes = Convert.FromBase64String(base64encodedstring.Split(',')[1]);
+            var imageFileStream = new MemoryStream(bytes);
+            
+            // Get a list of detected faces from Azure
+            IList<DetectedFace> faceList = 
+                    await faceClient.Face.DetectWithStreamAsync(
+                        imageFileStream, true, false, faceAttributes);
+
+            // Return the first face found
+            return faceList[0];
         }
 
-        async Task StartTimer()
-        {
-            WindowVisible = true;
-            for (int i = 0; i < 8; i++)
-            {
-                index = i;
-                StateHasChanged();
-                await Task.Delay(160);
-                await GetSnap();
-                StateHasChanged();
-                await Task.Delay(100);
-            }
-            WindowVisible = false;
-        }
+
 
     }
 }
---
+++ BlazorWebcamTest/Utilities.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebcamTest
+{
+    public static class Utilities
+    {
+        static readonly Random _random = new((int)DateTime.Now.Ticks);
+
+        /// <summary>
+        /// https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+        /// </summary>
+        public static List<T> Shuffle<T>(this IList<T> source)
+        {
+            List<T> result = new(source);
+            var count = result.Count;
+
+            while (count > 1)
+            {
+                --count;
+                var k = _random.Next(count + 1);
+                var value = result[k];
+                result[k] = result[count];
+                result[count] = value;
+            }
+
+            return result;
+        }
+
+        public static IEnumerable<T> Suffle<T>(this IEnumerable<T> source) => source.ToList().Shuffle();
+
+    }
+}
